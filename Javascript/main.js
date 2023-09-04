0498f213e7146df99a6b4f410993d2442779e6ac
KerolosAminFahmy <kerolosaminfahmy@gmail.com>
2023-09-05 02:05:45 +0300

    addHomePage

--- Javascript/main.js
+++ Javascript/main.js
@@ -3,6 +3,8 @@
 var iconButton=document.querySelector(".iconSearch");
 var SearchInput=document.getElementById("SearchInput");
 var isOpen=false;
+var isOpen1=false;
+
 iconButton.addEventListener("click",()=>{
   SearchInput.style.opacity="1";
   SearchInput.style.width="250px";
@@ -10,12 +12,19 @@ iconButton.addEventListener("click",()=>{
   iconButton.style.zIndex = '-1';
   isOpen=true;
 })
+var counter=0
 SearchInput.addEventListener("mouseout",()=>{
-  if (SearchInput.value.trim() === '') {
+  if (SearchInput.value.trim() === '' && isOpen1) {
+    counter=0;
+    isOpen1=false;
     SearchInput.style.opacity="0";
     SearchInput.style.width="0px";
     iconButton.style.opacity="1";
     iconButton.style.zIndex = '1';
+  }else if(counter==1){
+    isOpen1=true;
+  }else{
+    counter++;
   }
 })
 /* End Aimation Search Bar */
@@ -31,3 +40,16 @@ window.onload=()=>{
   BackgroundWaveBottomLeft.classList.add("ActiveBack");
 }
 /* End Intro Animation  */
+const textchanges=document.querySelectorAll(".TextChange");
+const MarkLine=document.querySelector(".MarkLine");
+textchanges.forEach((item)=>{
+  item.addEventListener("click",(e)=>{
+    e.preventDefault()
+    MarkLine.classList.toggle("activeAnton");
+    console.log(e)
+    textchanges.forEach(element => {
+      element.classList.toggle("ActiveText");
+    });
+
+  })
+})
