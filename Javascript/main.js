ec8429a1d275946fd863455d6ab727c7a385c296
KerolosAminFahmy <kerolosaminfahmy@gmail.com>
2023-09-04 20:10:48 +0300

    backgroundAnimation

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,3 +1,5 @@
+
+/*Aimation Search Bar */
 var iconButton=document.querySelector(".iconSearch");
 var SearchInput=document.getElementById("SearchInput");
 var isOpen=false;
@@ -10,9 +12,22 @@ iconButton.addEventListener("click",()=>{
 })
 SearchInput.addEventListener("mouseout",()=>{
   if (SearchInput.value.trim() === '') {
-  SearchInput.style.opacity="0";
-  SearchInput.style.width="0px";
-  iconButton.style.opacity="1";
-  iconButton.style.zIndex = '1';
-}
+    SearchInput.style.opacity="0";
+    SearchInput.style.width="0px";
+    iconButton.style.opacity="1";
+    iconButton.style.zIndex = '1';
+  }
 })
+/* End Aimation Search Bar */
+/* Start Intro Animation  */
+const header=document.querySelector("Header");
+const BackgroundWaveTop=document.getElementById("backgroundWaveTop");
+const BackgroundWaveBottom=document.getElementById("backgroundWaveBottom");
+const BackgroundWaveBottomLeft=document.getElementById("backgroundWaveBottomLeft");
+window.onload=()=>{
+  header.classList.add("ActiveHeader");
+  BackgroundWaveTop.classList.add("ActiveBack");
+  BackgroundWaveBottom.classList.add("ActiveBack");
+  BackgroundWaveBottomLeft.classList.add("ActiveBack");
+}
+/* End Intro Animation  */
