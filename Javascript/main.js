e40a334d7943bcaeea788503837d6ffdaeffd827
KerolosAminFahmy <kerolosaminfahmy@gmail.com>
2023-09-06 03:17:45 +0300

    AddHomeCard

--- Javascript/main.js
+++ Javascript/main.js
@@ -38,6 +38,9 @@ window.onload=()=>{
   BackgroundWaveTop.classList.add("ActiveBack");
   BackgroundWaveBottom.classList.add("ActiveBack");
   BackgroundWaveBottomLeft.classList.add("ActiveBack");
+  const div123 = document.getElementById('MyDivId'); 
+  const divHeight = div123.offsetHeight;
+  document.querySelector(".SectionKero").style.height=`${divHeight+30}px`;
 }
 /* End Intro Animation  */
 const textchanges=document.querySelectorAll(".TextChange");
@@ -53,3 +56,64 @@ textchanges.forEach((item)=>{
 
   })
 })
+
+
+var AllSeeMoreButton=document.querySelectorAll(".SeeMore");
+var AllSeeLessButton=document.querySelectorAll(".SeeLess");
+var AllDescription=document.querySelectorAll(".Description");
+console.log(window.innerWidth)
+if(window.innerWidth>=991){
+  AllSeeMoreButton.forEach((item)=>{
+    item.addEventListener("click",()=>{
+      const index = Array.from(AllSeeMoreButton).indexOf(item);
+      if(index%2==0){
+        item.classList.add("ActiveButtonRight");
+        AllDescription[index].classList.remove("AntonHideLeftSide");
+        AllDescription[index].style.display="block";
+        setTimeout(()=>{
+          AllSeeLessButton[index].style.transform = 'translateX(0)';
+        },100)
+      }
+    })
+  })
+  AllSeeLessButton.forEach((item)=>{
+    item.addEventListener("click",()=>{
+      const index = Array.from(AllSeeLessButton).indexOf(item);
+      if(index%2==0){
+        AllSeeLessButton[index].style.transform = 'translateX(-650px)';
+        AllDescription[index].classList.add("AntonHideLeftSide");
+        setTimeout(()=>{
+          AllDescription[index].style.display="none";
+          AllSeeLessButton[index].style.transform = 'translateX(-450px)';
+        },2000)
+        setTimeout(()=>{
+          AllSeeMoreButton[index].classList.remove("ActiveButtonRight");
+        },1200)
+      }
+    })
+  })
+}else{
+  AllSeeMoreButton.forEach((item)=>{
+    item.addEventListener("click",()=>{
+      const index = Array.from(AllSeeMoreButton).indexOf(item);
+        item.classList.add("ActiveButtonRight");
+        AllDescription[index].classList.remove("AntonHideLeftSide");
+        AllDescription[index].style.display="block";
+          AllSeeLessButton[index].style.transform = 'translateX(0)';
+
+    })
+  })
+  AllSeeLessButton.forEach((item)=>{
+    item.addEventListener("click",()=>{
+      const index = Array.from(AllSeeLessButton).indexOf(item);
+        AllDescription[index].classList.add("AntonHideLeftSide");
+        setTimeout(()=>{
+          AllDescription[index].style.display="none";
+          AllSeeLessButton[index].style.transform = 'translateX(-450px)';
+        },2000)
+        setTimeout(()=>{
+          AllSeeMoreButton[index].classList.remove("ActiveButtonRight");
+        },1300)
+    })
+  })
+}
